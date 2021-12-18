0fcfc21e622d4876731f0fdc2c55958eccf0f4c7
guerino <guerantonini94@gmail.com>
2021-12-17 19:07:48 -0500

    set up files for components

---
+++ src/components/DockList.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function DockList() {
+    return (
+        <div>
+            <h1>Docking</h1>
+        </div>
+    )
+}
+
+export default DockList
---
+++ src/components/VessleList.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function VesselList() {
+    return (
+        <div>
+            <h1>Vessels</h1>
+        </div>
+    )
+}
+
+export default VesselList
