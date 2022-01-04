881f52ff410dfb06d1bbb58f673d6f4122a56aee
guerino <guerantonini94@gmail.com>
2022-01-04 11:45:42 -0500

    Rendered vessels

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react"
 import VesselList from "./components/VessleList";
+import AddVessle from "./components/AddVessle"
 
 function App() {
 
   return (
     <div>
       <VesselList />
+
     </div>
 
 
--- src/components/VessleList.js
+++ src/components/VessleList.js
@@ -1,9 +1,48 @@
 import React from "react";
+import { useEffect, useState } from "react";
+import AddVessle from "./AddVessle";
+
+
+
+
+
+
 
 function VesselList() {
+
+    const [vessles, setVessles] = useState([])
+
+
+
+    useEffect(() => {
+        fetch("http://localhost:3001/vessles")
+            .then((resp) => resp.json())
+            .then(vessles => setVessles(vessles))
+    }, [])
+
+    function handleAddVessle(newVessle) {
+        setVessles([...vessles, newVessle])
+    }
+
+
+
+
+
+
     return (
         <div>
-            <h1>Vessels</h1>
+            <AddVessle onAddVessle={handleAddVessle} />
+            <br />
+            <ul className="Vessles">
+                {vessles.map((vessle) => (
+                    <li key={vessle.id}>
+
+                        {vessle.vessleName}
+                    </li>
+
+                ))}
+
+            </ul>
         </div>
     )
 }
